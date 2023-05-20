c1455502b6b56b57944d7ba473335073502af381
amaarirare <amaarirare@gmail.com>
2023-05-20 12:32:07 +0530

    update

--- ml/neurons_hidden_layers.py
+++ ml/neurons_hidden_layers.py
@@ -7,7 +7,6 @@ class Neuron:
     def __init__(self, weights, bias):                      # Neuron, [w1, w2], b
         self.weights = weights                              # w1, w2
         self.bias = bias                                    # b
-
     def feedforward(self, inputs):
         total = np.dot(self.weights, inputs) + self.bias    # y = (w . x) + b 
         return sigmoid(total)                               # S(y)
@@ -22,22 +21,17 @@ class NeuralNetwork:
         - w = [0, 1]
         - b = 0
     '''
-
     def __init__(self):
         weights = np.array([0, 1])                          # w1 = 0, w2 = 1
         bias = 0                                            # b = 0
-
         self.h1 = Neuron(weights, bias)
         self.h2 = Neuron(weights, bias)
         self.o1 = Neuron(weights, bias)
-
     def feedforward(self, x):
         out_h1 = self.h1.feedforward(x)
         out_h2 = self.h2.feedforward(x)
-
         # The inputs for o1 are the outputs from h1 and h2
         out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
-
         return out_o1
 
 network = NeuralNetwork()
