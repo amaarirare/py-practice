939c767fa8f57a6f6a4a010983037ec7cd76d384
fibbbonachos <arish2k3@gmail.com>
2023-08-11 14:29:20 +0530

    update

--- pytorch/tensor_operations.py
+++ pytorch/tensor_operations.py
@@ -1,7 +1,23 @@
 import torch
 import numpy as np
 
-tensor = torch.ones(4, 4)
+tensor = torch.rand(3, 3)
 
 if torch.cuda.is_available():
     tensor = tensor.to('cuda')
+
+# y0 = tensor 
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+# z1 = tensor * tensor
+
+# print(y0)
+# print(y1)
+# print(y2)
+
+# print(z1)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+
