690a3a431f3df503ce59fd6e4ab844e072bc1580
fibbbonachos <arish2k3@gmail.com>
2023-08-12 01:15:35 +0530

    update

--- pytorch/tensor_operations.py
+++ pytorch/tensor_operations.py
@@ -5,20 +5,3 @@ tensor = torch.rand(3, 3)
 
 if torch.cuda.is_available():
     tensor = tensor.to('cuda')
-
-# n = t.numpy()
-# t.add_(1)
-# print(n)
-
-# n = np.ones(5)
-# t = torch.from_numpy(n)
-
-# np.add(n, 1, out=n)
-# print(t)
-# print(n)
-
-# print(tensor)
-
-# agg = tensor.sum()
-# agg_item = agg.item()
-# print(agg_item, type(agg_item))
