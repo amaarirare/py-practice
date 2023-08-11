b200c3ec73fe37c640418bb299384831a7d7a5db
fibbbonachos <arish2k3@gmail.com>
2023-08-11 12:49:10 +0530

    update

--- pytorch/tensors.py
+++ pytorch/tensors.py
@@ -2,4 +2,12 @@ import torch
 import numpy as np
 
 data = [[1, 2], [3, 4]]
-x_data = torch.tensor(data)
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+print(np_array)
+print(x_np)
+
+x_ones = torch.ones_like(x_np)
+x_rand = torch.rand_like(x_np, dtype=torch.float)
+print(x_ones)
+print(x_rand)
